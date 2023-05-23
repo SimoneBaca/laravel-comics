c2590f4837093948e08517016968356ff7f9bd28
Simonebaca <simobaco99@gmail.com>
2023-05-23 18:13:56 +0200

    imp layout

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,17 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    @vite('resources/js/app.js')
-</head>
-
-<body>
-    @include("partials.header")
-    @include("partials.footer")
-</body>
-
-</html>
+@extends('layouts.first')
+@section('content')
+<div class="bg-darkgray">
+    <div class="pt-5 container position-relative">
+        <div class="position-absolute serie">
+            <h5 class="white m-0">CURRENT SERIES</h5>
+        </div>
+        <div class="row g-5">
+            @foreach (config("conf.comics") as $fumetto)
+            <div class="col-xl-2 col-3">
+                <div class="carta">
+                    <img class="img-comic mb-3" src="{{$fumetto['thumb']}}" alt="">
+                    <span class="white"></span>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="d-flex justify-content-center mt-5 pb-4">
+            <button class="white btn">LOAD MORE</button>
+        </div>
+    </div>
+</div>
+<div class="bg-blue">
+    <div class="p-5 row container m-auto">
+        <div class="acquisto d-flex align-items-center justify-content-center">
+            <img :src="acquisto.img">
+            <span class="white p-2"></span>
+        </div>
+    </div>
+</div>
+@endsection
